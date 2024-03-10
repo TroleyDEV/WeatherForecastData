e33dc7d81c494b4ab5634ba7c538366862759e62
Troley <troleyhs@gmail.com>
2024-03-10 23:40:47 +0100

    Fixed frontend

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import streamlit as st
 
 # UI
 st.title("Weather Forecast for the Next Days")
+
 place = st.text_input("Place:")
-st.slider(label="Forecast Days", min_value=1, max_value=5)
-st.selectbox(label="Select data to view", options="")
-st.title("Temperature for the next day in location TBD")
+
+days = st.slider(label="Forecast Days", min_value=1, max_value=5, help="Select the number of forecasted days")
+
+option = st.selectbox(label="Select data to view", options=("Temperature", "Sky"))
+
+st.subheader(f"{option} for the next {days} days in {place}")
